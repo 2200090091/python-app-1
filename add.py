eedd899a795cb038af7aa07a32e3f498f589dcde
Baggu <2200090091@kluniversity.in>
2024-01-11 14:05:04 +0530

    mycommit

---
+++ add.py
@@ -0,0 +1,6 @@
+this is my java program[D[D[D[D[D[D[c program pytghon file[D[D[D[D[D[D[D[[[C[C[hon[C[[C[C[C[C[C
+int sum1,sum2,sum;
+scanf &sum1,sum2
+printfenter[D[D[D[D[D enter the values of sum1 and sum2
+sum = sum1+sum2
+printf sum
